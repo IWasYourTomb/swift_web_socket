81b0207d2d289570164952dfe1977c8a1adc084d
wowzik1233 <wowzik1233@gmail.com>
2024-04-08 23:04:10 +0300

    Initial Commit

---
+++ WebSocketApp/WebSocketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketAppApp.swift
+//  WebSocketApp
+//
+//  Created by matvey on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
